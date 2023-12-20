31b3896a097b3da01fe44860444060bf8e6e235d
Stefan Muscat Doublesin <148441668+stefanmdkpmg@users.noreply.github.com>
2023-12-20 11:37:27 +0100

    Update setupvm.ps1

--- collated/setupvm.ps1
+++ collated/setupvm.ps1
@@ -1,4 +1,144 @@
 ################################################################################
+##  File:  
+##  Desc:  Functions
+################################################################################
+
+function Install-Binary {
+    <#
+    .SYNOPSIS
+        A function to install binaries from either a URL or a local path.
+
+    .DESCRIPTION
+        This function downloads and installs .exe or .msi binaries from a specified URL or a local path. It also supports checking the binary's signature and SHA256/SHA512 sum before installation.
+
+    .PARAMETER Url
+        The URL from which the binary will be downloaded. This parameter is required if LocalPath is not specified.
+
+    .PARAMETER LocalPath
+        The local path of the binary to be installed. This parameter is required if Url is not specified.
+
+    .PARAMETER Type
+        The type of the binary to be installed. Valid values are "MSI" and "EXE". If not specified, the type is inferred from the file extension.
+
+    .PARAMETER InstallArgs
+        The list of arguments that will be passed to the installer. Cannot be used together with ExtraInstallArgs.
+
+    .PARAMETER ExtraInstallArgs
+        Additional arguments that will be passed to the installer. Cannot be used together with InstallArgs.
+
+    .PARAMETER ExpectedSignature
+        The expected signature of the binary. If specified, the binary's signature is checked before installation.
+
+    .PARAMETER ExpectedSHA256Sum
+        The expected SHA256 sum of the binary. If specified, the binary's SHA256 sum is checked before installation.
+
+    .PARAMETER ExpectedSHA512Sum
+        The expected SHA512 sum of the binary. If specified, the binary's SHA512 sum is checked before installation.
+
+    .EXAMPLE
+        Install-Binary -Url "https://go.microsoft.com/fwlink/p/?linkid=2083338" -Type EXE -InstallArgs ("/features", "+", "/quiet") -ExpectedSignature "A5C7D5B7C838D5F89DDBEDB85B2C566B4CDA881F"
+    #>
+
+    Param
+    (
+        [Parameter(Mandatory, ParameterSetName = "Url")]
+        [String] $Url,
+        [Parameter(Mandatory, ParameterSetName = "LocalPath")]
+        [String] $LocalPath,
+        [ValidateSet("MSI", "EXE")]
+        [String] $Type,
+        [String[]] $InstallArgs,
+        [String[]] $ExtraInstallArgs,
+        [String[]] $ExpectedSignature,
+        [String] $ExpectedSHA256Sum,
+        [String] $ExpectedSHA512Sum
+    )
+
+    if ($PSCmdlet.ParameterSetName -eq "LocalPath") {
+        if (-not (Test-Path -Path $LocalPath)) {
+            throw "LocalPath parameter is specified, but the file does not exist."
+        }
+        if (-not $Type) {
+            $Type = ([System.IO.Path]::GetExtension($LocalPath)).Replace(".", "").ToUpper()
+            if ($Type -ne "MSI" -and $Type -ne "EXE") {
+                throw "LocalPath parameter is specified, but the file extension is not .msi or .exe. Please specify the Type parameter."
+            }
+        }
+        $filePath = $LocalPath
+    } else {
+        if (-not $Type) {
+            $Type = ([System.IO.Path]::GetExtension($Url)).Replace(".", "").ToUpper()
+            if ($Type -ne "MSI" -and $Type -ne "EXE") {
+                throw "Cannot determine the file type from the URL. Please specify the Type parameter."
+            }
+            $fileName = [System.IO.Path]::GetFileName($Url)
+        } else {
+            $fileName = [System.IO.Path]::GetFileNameWithoutExtension([System.IO.Path]::GetRandomFileName()) + ".$Type".ToLower()
+        }
+        $filePath = Invoke-DownloadWithRetry -Url $Url -Path "${env:Temp}\$fileName"
+    }
+
+    if ($PSBoundParameters.ContainsKey('ExpectedSignature')) {
+        if ($ExpectedSignature) {
+            Test-FileSignature -Path $filePath -ExpectedThumbprint $ExpectedSignature
+        } else {
+            throw "ExpectedSignature parameter is specified, but no signature is provided."
+        }
+    }
+
+    if ($ExpectedSHA256Sum) {
+        Test-FileChecksum $filePath -ExpectedSHA256Sum $ExpectedSHA256Sum
+    }
+
+    if ($ExpectedSHA512Sum) {
+        Test-FileChecksum $filePath -ExpectedSHA512Sum $ExpectedSHA512Sum
+    }
+
+    if ($ExtraInstallArgs -and $InstallArgs) {
+        throw "InstallArgs and ExtraInstallArgs parameters cannot be used together."
+    }
+ 
+    if ($Type -eq "MSI") {
+        # MSI binaries should be installed via msiexec.exe
+        if ($ExtraInstallArgs) {
+            $InstallArgs = @('/i', $filePath, '/qn', '/norestart') + $ExtraInstallArgs
+        } elseif (-not $InstallArgs) {
+            Write-Host "No arguments provided for MSI binary. Using default arguments: /i, /qn, /norestart"
+            $InstallArgs = @('/i', $filePath, '/qn', '/norestart')
+        }
+        $filePath = "msiexec.exe"
+    } else {
+        # EXE binaries should be started directly
+        if ($ExtraInstallArgs) {
+            $InstallArgs = $ExtraInstallArgs
+        }
+    }
+
+    $installStartTime = Get-Date
+    Write-Host "Starting Install $Name..."
+    try {
+        $process = Start-Process -FilePath $filePath -ArgumentList $InstallArgs -Wait -PassThru
+        $exitCode = $process.ExitCode
+        $installCompleteTime = [math]::Round(($(Get-Date) - $installStartTime).TotalSeconds, 2)
+        if ($exitCode -eq 0) {
+            Write-Host "Installation successful in $installCompleteTime seconds"
+        } elseif ($exitCode -eq 3010) {
+            Write-Host "Installation successful in $installCompleteTime seconds. Reboot is required."
+        } else {
+            Write-Host "Installation process returned unexpected exit code: $exitCode"
+            Write-Host "Time elapsed: $installCompleteTime seconds"
+            exit $exitCode
+        }
+    } catch {
+        $installCompleteTime = [math]::Round(($(Get-Date) - $installStartTime).TotalSeconds, 2)
+        Write-Host "Installation failed in $installCompleteTime seconds"
+    }
+}
+
+
+
+
+################################################################################
 ##  File:  Configure-Powershell.ps1
 ##  Desc:  Manage PowerShell configuration
 ################################################################################
