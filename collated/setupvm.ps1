77a3b094d96d0b35acabb0dda4890246a3ac3636
Stefan Muscat Doublesin <148441668+stefanmdkpmg@users.noreply.github.com>
2023-12-20 11:10:31 +0100

    adding sections

--- collated/setupvm.ps1
+++ collated/setupvm.ps1
@@ -1,1 +1,70 @@
+################################################################################
+##  File:  Configure-Powershell.ps1
+##  Desc:  Manage PowerShell configuration
+################################################################################
 
+#region System
+Write-Host "Setup PowerShellGet"
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
+# Specifies the installation policy
+Set-PSRepository -InstallationPolicy Trusted -Name PSGallery
+
+Write-Host 'Warmup PSModuleAnalysisCachePath (speedup first powershell invocation by 20s)'
+$PSModuleAnalysisCachePath = 'C:\PSModuleAnalysisCachePath\ModuleAnalysisCache'
+
+[Environment]::SetEnvironmentVariable('PSModuleAnalysisCachePath', $PSModuleAnalysisCachePath, "Machine")
+# make variable to be available in the current session
+${env:PSModuleAnalysisCachePath} = $PSModuleAnalysisCachePath
+
+New-Item -Path $PSModuleAnalysisCachePath -ItemType 'File' -Force | Out-Null
+#endregion
+
+#region User (current user, image generation only)
+if (-not (Test-Path $profile)) {
+    New-Item $profile -ItemType File -Force
+}
+  
+@" 
+  if ( -not(Get-Module -ListAvailable -Name PowerHTML)) {
+      Install-Module PowerHTML -Scope CurrentUser 
+  } 
+  
+  if ( -not(Get-Module -Name PowerHTML)) {
+      Import-Module PowerHTML
+  } 
+"@ | Add-Content -Path $profile -Force
+
+#endregion
+
+
+
+################################################################################
+##  File:  Install-AzureCli.ps1
+##  Desc:  Install and warm-up Azure CLI
+################################################################################
+
+Write-Host 'Install the latest Azure CLI release'
+
+$azureCliConfigPath = 'C:\azureCli'
+# Store azure-cli cache outside of the provisioning user's profile
+[Environment]::SetEnvironmentVariable('AZURE_CONFIG_DIR', $azureCliConfigPath, "Machine")
+
+$azureCliExtensionPath = Join-Path $env:CommonProgramFiles 'AzureCliExtensionDirectory'
+New-Item -ItemType 'Directory' -Path $azureCliExtensionPath | Out-Null
+[Environment]::SetEnvironmentVariable('AZURE_EXTENSION_DIR', $azureCliExtensionPath, "Machine")
+
+Install-Binary -Type MSI `
+    -Url 'https://aka.ms/installazurecliwindowsx64' `
+    -ExpectedSignature '72105B6D5F370B62FD5C82F1512F7AD7DEE5F2C0'
+
+Update-Environment
+
+# Warm-up Azure CLI
+Write-Host "Warmup 'az'"
+az --help | Out-Null
+if ($LASTEXITCODE -ne 0) {
+    throw "Command 'az --help' failed"
+}
+
+Invoke-PesterTests -TestFile 'CLI.Tools' -TestName 'Azure CLI'
