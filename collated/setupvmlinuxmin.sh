36a6407051d87674a02013c6e8446c74e3a94e1d
Stefan Muscat Doublesin <148441668+stefanmdkpmg@users.noreply.github.com>
2023-12-22 14:44:59 +0100

    Update setupvmlinuxmin.sh

--- collated/setupvmlinuxmin.sh
+++ collated/setupvmlinuxmin.sh
@@ -2,23 +2,11 @@
 
 sudo apt install unzip
 
-################################################################################
-##  File:  install-azure-cli.sh
-##  Desc:  Install Azure CLI (az)
-################################################################################
-
 # Install Azure CLI (instructions taken from https://docs.microsoft.com/en-us/cli/azure/install-azure-cli)
 curl -fsSL https://aka.ms/InstallAzureCLIDeb | sudo bash
 echo "azure-cli https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt" >> $HELPER_SCRIPTS/apt-sources.txt
 sudo rm -f /etc/apt/sources.list.d/azure-cli.list
 sudo rm -f /etc/apt/sources.list.d/azure-cli.list.save
-
-################################################################################
-##  File:  install-azure-devops-cli.sh
-##  Desc:  Install Azure DevOps CLI (az devops)
-################################################################################
-
-# Source the helpers for use with the script
 
 # AZURE_EXTENSION_DIR shell variable defines where modules are installed
 # https://docs.microsoft.com/en-us/cli/azure/azure-cli-extensions-overview
