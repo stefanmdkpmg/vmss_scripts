0d5755d63db946e27378c15fe67ade613d08f943
Stefan Muscat Doublesin <148441668+stefanmdkpmg@users.noreply.github.com>
2023-12-22 09:48:00 +0100

    Update setupvm.ps1

--- collated/setupvm.ps1
+++ collated/setupvm.ps1
@@ -1256,4 +1256,7 @@ Dismount-RegistryHive "HKLM\DEFAULT"
 
 Write-Host "Configure-User.ps1 - completed"
 
+# create user profile at the first launch
+$null = cmd /c "C:\msys64\usr\bin\bash.exe -leo pipefail -c 'echo $SHELL' 2>&1"
+
 
