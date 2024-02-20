04cf1ae6d0a907acda7e15fe9072d1979caf1370
Stefan Muscat Doublesin <148441668+stefanmdkpmg@users.noreply.github.com>
2024-02-20 09:30:18 +0100

    Update setupvmlinuxmin.sh

--- collated/setupvmlinuxmin.sh
+++ collated/setupvmlinuxmin.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+exit 1
 
 # Install Azure CLI (instructions taken from https://docs.microsoft.com/en-us/cli/azure/install-azure-cli)
 curl -fsSL https://aka.ms/InstallAzureCLIDeb | sudo bash
